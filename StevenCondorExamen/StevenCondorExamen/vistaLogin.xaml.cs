9b951ab528c7b39c63213f11f946f5434e5580f6
Steven Condor <stev.alejo.1@gmail.com>
2020-10-26 23:41:45 -0500

    Examen - proyectos de desarrollo movil

---
+++ StevenCondorExamen/StevenCondorExamen/vistaLogin.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StevenCondorExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class vistaLogin : ContentPage
+    {
+        public vistaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        { 
+            string user = "estudiante2020";
+            string passwd = "uisrael2020";
+            string usuario = txtUsuario.Text;
+            string password = txtPasswd.Text;
+
+            try
+            {
+                if (user == usuario && passwd == password)
+                {
+                    await Navigation.PushAsync(new MainPage(user, passwd));
+                }
+                else
+                {
+                    string mensaje = "El usuario o la contraseña no son validos.";
+                    await DisplayAlert("Mensaje de Alerta", mensaje, "Aceptar");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Mensaje de Alerta", ex.Message, "Aceptar");
+            }
+        }
+    }
+}
---
+++ StevenCondorExamen/StevenCondorExamen/vistaResumen.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StevenCondorExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class vistaResumen : ContentPage
+    {
+        public vistaResumen(string nombre, string usuario, string cuotas)
+        {
+            double diferencia = Convert.ToDouble(cuotas)*3;
+            InitializeComponent();
+            lblUsuario.Text = "Bienvenido: " + usuario;
+            txtNombre.Text = nombre;
+            txtCuotas.Text = cuotas.ToString();
+            txtTotak.Text = diferencia.ToString();
+        }
+    }
+}
